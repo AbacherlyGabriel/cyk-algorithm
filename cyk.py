8e5cb06c2e1651a909398f2c833c03483b65b122
AbacherlyGabriel <gaabacherly@usp.br>
2020-12-15 11:41:39 -0300

    Starting CYK implementation

--- cyk.py
+++ cyk.py
@@ -24,6 +24,7 @@ class CYK:
         index_variaveis = 2                                                                         # Indice das variaveis das GLCs na lista de entrada
         index_terminais = 3                                                                         # Indice dos terminais das GLCs na lista de entrada
         index_regras = 4                                                                            # Indice das regras das GLCs na lista de entrada
+        index_cadeias = 0                                                                           # Indice da quantidade de cadeias correspondentes a primeira GLC
 
         qtd_glcs = int(self.gramaticas[0])                                                          # Quantidade de GLCs
 
@@ -34,10 +35,7 @@ class CYK:
             terminais = self.gramaticas[index_terminais].split()                                                    # Terminais das GLCs
             regras = self.gramaticas[index_regras:index_regras + qtd_regras]                                        # Regras das GLCs
 
-            index_especs += qtd_regras + 3                                                                          # Atualizando indice
-            index_variaveis += qtd_regras + 3                                                                       # Atualizando indice
-            index_terminais += qtd_regras + 3                                                                       # Atualizando indice
-            index_regras += qtd_regras + 3                                                                          # Atualizando indice
+            qtd_cadeias = int(self.cadeias[index_cadeias])                                                          # Quantidade de cadeias de teste
 
             print(f'\nglc {glc}')
             print(f'qtd variaveis: {qtd_variaveis}')
@@ -45,8 +43,34 @@ class CYK:
             print(f'qtd regras: {qtd_regras}')
             print(f'\nvariaveis: {variaveis}')
             print(f'terminais: {terminais}')
-            print(f'regras: {regras}')
+            print(f'regras: {regras}\n')
 
+            """
+            Leitura das cadeias de teste e execucao do algoritmo
+            """
+
+            while qtd_cadeias > 0:
+                index_cadeias += 1                                                                                  # Atualizando indice
+                qtd_cadeias -= 1                                                                                    # Atualizando quantidade de cadeias
+
+                cadeia = self.cadeias[index_cadeias].split()                                                        # Lista contendo os simbolos da cadeia
+
+                print(f'cadeia: {cadeia}')
+
+                """
+                Validando se o teste eh a cadeia vazia, alem de checar
+                se a substituicao faz parte do conjunto de regras
+                """
+
+                if ('&' in cadeia) and (len(cadeia) == 1):
+                    return True
+
+            index_especs += qtd_regras + 3                                                                          # Atualizando indice
+            index_variaveis += qtd_regras + 3                                                                       # Atualizando indice
+            index_terminais += qtd_regras + 3                                                                       # Atualizando indice
+            index_regras += qtd_regras + 3                                                                          # Atualizando indice
+            index_cadeias += 1                                                                                      # Atualizando indice
+            
 
 # Testes
-CYK().algoritmo_cyk()
+print(CYK().algoritmo_cyk())
